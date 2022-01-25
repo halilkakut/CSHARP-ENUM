24c09b63f7d118b6e3c11ced5e2254bb1f8afde5
Halil İbrahim Kakut <halil.kakut@hotmail.com>
2022-01-25 22:33:22 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CSHARP_ENUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sıcaklık = 32;
+            if(sıcaklık <= (int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya Çıkmak için havanın ısınmasını bekleyin");
+            else if(sıcaklık >= (int)HavaDurumu.Sıcak)
+                Console.WriteLine("Dışarıya Çıkmak için hava çok sıcak bir gün");
+            else if(sıcaklık >= (int)HavaDurumu.Normal && sıcaklık < (int)HavaDurumu.CokSıcak)
+                Console.WriteLine("Hadi Dışarı çıkalım");
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sıcak = 27,
+        CokSıcak = 35
+    }
+}
